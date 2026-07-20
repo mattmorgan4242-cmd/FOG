4d7732742904a36db611331c1cdbcadb5383d47c
MattMorgan <mattmorgan4242@gmail.com>
2026-07-20 16:30:20 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,13 @@
 int main() {
 InitWindow(800, 600, "sth");
 SetTargetFPS(60);
+ while (!WindowShouldClose()) {
+BeginDrawing();
+ClearBackground(BLACK);
+
+
+EndDrawing();
+}
 
 //CloseWindow();
 return 0;
